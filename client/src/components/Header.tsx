e503a83edc9108d58991d66d4c099cc50190b091
ketandabhi <ketan.dabhi@solutionanalysts.com>
2023-10-10 13:35:28 +0530

    protected profile page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { currentUser } = useSelector((state: any) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,6 +33,20 @@ const Header = () => {
             <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
+          </Link>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="hidden sm:inline text-slate-700 hover:underline">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
